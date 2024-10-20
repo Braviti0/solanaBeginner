829b778b0bae7bc358cf092977a55cbacf88a2c8
Agyeman <84294327+Braviti0@users.noreply.github.com>
2024-10-20 19:15:45 +0000

    Create firstTokenCreation.js

---
+++ module2/firstTokenCreation.js
@@ -0,0 +1,28 @@
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import { createMint, getOrCreateAssociatedTokenAccount, mintTo, transfer } from '@solana/spl-token';
+
+(async () => {
+    // Step 1: Connect to cluster and generate a new Keypair
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+    const fromWallet = Keypair.generate();
+    // Step 2: Airdrop SOL into your from wallet
+    const fromAirdropSignature = await connection.requestAirdrop(fromWallet.publicKey, 2* LAMPORTS_PER_SOL);
+    
+
+    
+    // Step 3: Create new token mint and get the token account of the fromWallet address
+    //If the token account does not exist, create it
+
+    
+    
+    //Step 4: Mint a new token to the from account
+    
+
+    //Step 5: Get the token account of the to-wallet address and if it does not exist, create it
+    
+
+    //Step 6: Transfer the new token to the to-wallet's token account that was just created
+    // Transfer the new token to the "toTokenAccount" we just created
+ 
+})();
